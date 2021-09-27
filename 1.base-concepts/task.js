cbec2017579edb45aa11a8583f8a3d462645c4c0
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-09-27 12:17:51 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,34 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+
+  
+  "use strict";
+
+let quadraticEquation = (a, b, c) => {
+ 
+    if(a == 0)
+        return false;
+    let res = {};
+    let D = b * b - 4 * a * c;
+    console.log('D = ' + D);
+    if(D < 0)
+        return false;
+    res['discriminant'] = D;
+    if(D == 0)
+        res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+    else if(D > 0){
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        res["quadratic roots"] = tmp;
+    }
+    return res;
+}
+console.log(quadraticEquation(5,7,1));
+
+  
+  
+  return res;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
