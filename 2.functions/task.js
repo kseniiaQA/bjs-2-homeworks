dc95510e84ccaf93d8b5debb837be5f1d85a47af
Kseniia Alekseenko <kseniya.a@devstark.com>
2021-09-28 13:17:41 +0300

    tests 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,39 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
-
-  // Ваш код
-
-  return { min:min, max:max, avg:avg };
+  let min = Infinity, max = -Infinity, sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  return { min: min, max: max, avg: +(sum / arr.length).toFixed(2) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
-  
-  return max
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    }
+  }
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let result = getArrayParams(arr);
+  return Math.abs(result.max - result.min);
 }
