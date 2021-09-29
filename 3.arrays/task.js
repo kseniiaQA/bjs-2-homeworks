be950703e4926d1f99d7c24787c468213421029f
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-09-29 14:31:19 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
+
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  function isEqual(number, index) {
+    return number === arr2[index];
+  }
+  if (arr1.length != arr2.length) {
+    return false;
+  } else {
+    result = arr1.every(isEqual, arr2);
+  }
   return result; // boolean
 }
 
-function advancedFilter(arr) {
+ function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(number => (number > 0) && (number % 3 === 0)).map(number => number = number * 10);
   return resultArr; // array
 }
