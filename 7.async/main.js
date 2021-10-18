1c9e5930df74e88fd038288bf08741092f537430
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-10-18 15:41:47 +0300

    Update main.js

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,22 @@
-// тут вы можете вызывать функции из task.js
+function testCase() {
+    clock = new AlarmClock();
+    clock.addClock("16:45", () => console.log(`Первый звонок.`), 1);
+
+    clock.addClock("16:46", () => {
+        console.log(`Второй звонок.`);
+        clock.removeClock(2);
+    }, 2);
+
+    clock.addClock("16:47", () => {
+        console.log(`Третий звонок.`);
+        clock.stop();
+        clock.clearAlarms();
+        clock.printAlarms();
+    }, 3);
+
+    clock.printAlarms();
+
+    clock.start();
+}
+
+testCase();
