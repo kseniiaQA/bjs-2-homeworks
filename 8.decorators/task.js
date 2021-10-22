bdbb51582c2437f8c8d41d8a6e986a9c62a976d8
kseniiaQA <82049513+kseniiaQA@users.noreply.github.com>
2021-10-22 10:42:42 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,47 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  return function (...args) {
+    hash = args.join(',');
+    if (hash in cache) {
+      return "Из кэша: " + cache[hash];
+    } else if (Object.keys(cache).length === 5) {
+      delete cache[Object.keys(cache)[0]];
+      cache[hash] = func(...args);
+      return "Вычисляем: " + cache[hash];
+    } else {
+      cache[hash] = func(...args);
+      return "Вычисляем: " + cache[hash];
+    }
+  };
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let flag;
+  return function () {
+    if (flag) {
+      return;
+    }
+    func();
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+    }, ms);
+  }
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let flag;
+  function withCounter() {
+    withCounter.count++;
+    if (flag) {
+      return;
+    }
+    func();
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+    }, ms);
+  }
+  withCounter.count = 0;
+  return withCounter;
 }
